ca5e49f8aa60f8b8c56d38032aaca19bc2f5a83c
AnnoyingBird <kennadi@my.prideprepschool.org>
2017-05-01 13:13:19 -0700

    HW2- Linked and Changes Label. NEXT: Colors, Hide Buttons?

--- MidSemApp/MidSemApp/ViewController.swift
+++ MidSemApp/MidSemApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Label
+    @IBOutlet weak var Main: UILabel!
+    
+    
+    //Preload
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//Buttons
+    @IBAction func Kill_Button(_ sender: Any) {
+        Main.text = "You and your friends kill Kaladin with magic, that rebounds and kill you all as well. THE END"
+    }
 
-
+    @IBAction func Surrender_Button(_ sender: Any) {
+        Main.text = "You surrender, and you and your friends are brutally murdered. THE END"
+    }
+    
+    @IBAction func Join_Button(_ sender: Any) {
+        Main.text = "You stand beside Kaladin, and watch in silence as he murders your friends in front of your eyes. Someone sneaks up behind you and kills both you and Kaladin. THE END. "
+    }
+    
+    @IBAction func Persuade_Button(_ sender: Any) {
+        Main.text = "You convince Kaladin to be good. You peacefully fade into fireflies and fly off into the sunset after a heartbreaking conversation with your best friend. THIS IS NOT THE END"
+    }
 }
 
