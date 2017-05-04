1de0a02b701b0fbe92cf7a266043aaf02ff67818
AnnoyingBird <kennadi@my.prideprepschool.org>
2017-05-04 13:10:39 -0700

    HW2- Almost Revised. Hidden main buttons. Finish Restart Func

--- MidSemApp/MidSemApp/ViewController.swift
+++ MidSemApp/MidSemApp/ViewController.swift
@@ -13,11 +13,18 @@ class ViewController: UIViewController {
     //Label
     @IBOutlet weak var Main: UILabel!
     
+   //Outlet Buttons
+    @IBOutlet weak var Kill_Outlet: UIButton!
+    @IBOutlet weak var Surrender_Outlet: UIButton!
+    @IBOutlet weak var Join_Outlet: UIButton!
+    @IBOutlet weak var Persuade_Outlet: UIButton!
+    @IBOutlet weak var Restart_Outlet: UIButton!
     
     //Preload
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Restart_Outlet.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,18 +35,44 @@ class ViewController: UIViewController {
 //Buttons
     @IBAction func Kill_Button(_ sender: Any) {
         Main.text = "You and your friends kill Kaladin with magic, that rebounds and kill you all as well. THE END"
+        Restart_Outlet.isHidden = false
+        Kill_Outlet.isHidden = true
+        Surrender_Outlet.isHidden = true
+        Join_Outlet.isHidden = true
+        Persuade_Outlet.isHidden = true
     }
 
     @IBAction func Surrender_Button(_ sender: Any) {
         Main.text = "You surrender, and you and your friends are brutally murdered. THE END"
-    }
+        Restart_Outlet.isHidden = false
+        Kill_Outlet.isHidden = true
+        Surrender_Outlet.isHidden = true
+        Join_Outlet.isHidden = true
+        Persuade_Outlet.isHidden = true
+
+           }
     
     @IBAction func Join_Button(_ sender: Any) {
         Main.text = "You stand beside Kaladin, and watch in silence as he murders your friends in front of your eyes. Someone sneaks up behind you and kills both you and Kaladin. THE END "
+        Restart_Outlet.isHidden = false
+        Kill_Outlet.isHidden = true
+        Surrender_Outlet.isHidden = true
+        Join_Outlet.isHidden = true
+        Persuade_Outlet.isHidden = true
+
     }
     
     @IBAction func Persuade_Button(_ sender: Any) {
         Main.text = "You convince Kaladin to be good. You peacefully fade into fireflies and fly off into the sunset after a heartbreaking conversation with your best friend. THIS IS NOT THE END"
+        Restart_Outlet.isHidden = false
+        Kill_Outlet.isHidden = true
+        Surrender_Outlet.isHidden = true
+        Join_Outlet.isHidden = true
+        Persuade_Outlet.isHidden = true
+    }
+    
+    @IBAction func Restart_Button(_ sender: Any) {
+        Main.text = "Another option forms in your mind: Try to bring Kaladin over to your side, and stop the destruction of the world where everybody would die at the Chaos Moon. "
     }
 }
 
